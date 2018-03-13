1aff153321ae87a0c258d6a6c2ad8c6d7252babf
Gregor <gregor@martynus.net>
2018-03-13 16:14:44 -0700

    test: 2nd parameters table when type is `object`

---
+++ test/git/tags/create-a-tag-object/test.js
@@ -0,0 +1,17 @@
+const {resolve: resolvePath} = require('path')
+const {readFileSync} = require('fs')
+
+const {test} = require('tap')
+
+const endpointHtmlToJson = require('../../../../lib/endpoint/html-to-json')
+
+const fixtures = {
+  in: readFileSync(resolvePath(__dirname, 'in.html')),
+  out: require('./out.json')
+}
+
+test('https://developer.github.com/v3/git/tags/#create-a-tag-object to JSON', t => {
+  const actual = endpointHtmlToJson(fixtures.in)
+  t.deepEquals(actual, fixtures.out)
+  t.end()
+})
