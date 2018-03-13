5f07d056b61c852a1855221f6c8d76c1af2378b7
Gregor <gregor@martynus.net>
2018-03-13 12:03:06 -0700

    test: description after Default value

--- test/repos/deployments/create-a-deployment/test.js
+++ test/repos/deployments/create-a-deployment/test.js
@@ -3,14 +3,14 @@ const {readFileSync} = require('fs')
 
 const {test} = require('tap')
 
-const endpointHtmlToJson = require('../../../lib/endpoint/html-to-json')
+const endpointHtmlToJson = require('../../../../lib/endpoint/html-to-json')
 
 const fixtures = {
   in: readFileSync(resolvePath(__dirname, 'in.html')),
   out: require('./out.json')
 }
 
-test('https://developer.github.com/v3/pulls/#list-pull-requests to JSON', t => {
+test('https://developer.github.com/v3/repos/deployments/#create-a-deployment to JSON', t => {
   const actual = endpointHtmlToJson(fixtures.in)
   t.deepEquals(actual, fixtures.out)
   t.end()
