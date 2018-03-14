b86c7aac7009531e4d31750d6c4a1c878bdc5efc
Gregor <gregor@martynus.net>
2018-03-13 18:06:47 -0700

    test: multiple routes list starring activity section

---
+++ test/activity/starring/test.js
@@ -0,0 +1,17 @@
+const {resolve: resolvePath} = require('path')
+const {readFileSync} = require('fs')
+
+const {test} = require('tap')
+
+const endpointHtmlToJson = require('../../../lib/endpoint/html-to-json')
+
+const fixtures = {
+  in: readFileSync(resolvePath(__dirname, 'in.html')),
+  out: require('./out.json')
+}
+
+test('https://developer.github.com/v3/activity/starring/#list-repositories-being-starred to JSON', t => {
+  const actual = endpointHtmlToJson(fixtures.in)
+  t.deepEquals(actual, fixtures.out)
+  t.end()
+})
