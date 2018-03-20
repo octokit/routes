2ce5c58eb159ef406844494cdf89a5d04597afda
Gregor <gregor@martynus.net>
2018-03-19 20:29:21 -0700

    test: all endpoints in index, scope and single route files

---
+++ test/util.js
@@ -0,0 +1,59 @@
+const {flatten, kebabCase} = require('lodash')
+
+module.exports = {
+  getAllRoutesByScope,
+  getAllRoutesByDocumentUrl,
+  getAllDocumentationUrls,
+  getScopeRoutes,
+  getScopeRoutesByDocumentUrl,
+  getRoutesForUrl
+}
+
+const SCOPES = Object.keys(getAllRoutesByScope())
+
+function getAllRoutesByScope () {
+  return require('../index.json')
+}
+
+const CACHED_ROUTES_BY_DOCUMENTATION_URL = flatten(
+  SCOPES.map(scope => getAllRoutesByScope()[scope])
+).reduce(reduceByDocumentationUrl, {})
+function getAllRoutesByDocumentUrl () {
+  return CACHED_ROUTES_BY_DOCUMENTATION_URL
+}
+
+function getAllDocumentationUrls () {
+  if (process.env.TEST_URL) {
+    return process.env.TEST_URL.split(/\s*,\s/)
+  }
+
+  return Object.keys(getAllRoutesByDocumentUrl())
+}
+
+function getScopeRoutes (scope) {
+  return require(`../routes/${kebabCase(scope)}.json`)
+}
+
+function getScopeRoutesByDocumentUrl (scope) {
+  return getScopeRoutes(scope).reduce(reduceByDocumentationUrl, {})
+}
+
+function getRoutesForUrl (url) {
+  const matches = url.match(/\/v3\/([^/#]+)((\/[^/#]+)*)/)
+  const scope = kebabCase(matches[1])
+  const path = kebabCase(matches[2])
+  const routes = CACHED_ROUTES_BY_DOCUMENTATION_URL[url]
+  const names = routes.map(route => kebabCase(route.name).replace(/\bgit-hub\b/, 'github'))
+
+  return names.map(name => require(`../routes/${scope}/${path}/${name}.json`))
+}
+
+function reduceByDocumentationUrl (map, endpoint) {
+  const documentationUrl = endpoint.documentationUrl
+  if (!map[documentationUrl]) {
+    map[documentationUrl] = []
+  }
+  map[documentationUrl].push(endpoint)
+
+  return map
+}
