97aee804d5f133c4871d2f1e0492071f338a7542
Derek <derek.dunagan@gmail.com>
2019-06-27 17:16:06 +0200

    #443 add ci test battery

---
+++ bin/run-tests.js
@@ -0,0 +1,28 @@
+#!/usr/bin/env node
+
+const { readdirSync } = require('fs')
+const { spawn } = require('tap')
+
+spawn(
+  'tap',
+  [ '--no-coverage', '--timeout=60', 'test/unit/*-test.js' ],
+  {
+    name: 'unit tests',
+    env: { PATH: process.env.PATH }
+    //     ^ might be unnecessary in ci?
+  }
+)
+
+const apis = readdirSync('routes')
+for (const api of apis) {
+  const GHE_VERSION = parseFloat(api.replace(/^ghe-(\d+\.\d+)$/, '$1')) || null
+  spawn(
+    'tap',
+    [ '--no-coverage', '--timeout=60', 'test/integration/*-test.js' ],
+    {
+      name: `${api} integration tests`,
+      env: { GHE_VERSION: GHE_VERSION, PATH: process.env.PATH }
+      //                               ^ might be unnecessary in ci?
+    }
+  )
+}
