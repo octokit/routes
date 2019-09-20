cecd2adbe90afbc5bb08537494197780fcd72f35
Gregor Martynus <gregor@martynus.net>
2019-09-20 15:58:56 -0700

    ci(update): fail with helpful error when environment variables are missing

--- bin/create-pull-request-on-change.js
+++ bin/create-pull-request-on-change.js
@@ -1,8 +1,21 @@
 #!/usr/bin/env node
 
-const checkOrUpdateRoutes = require("../lib/notify-about-routes-changes");
+const notifyAboutRoutesChanges = require("../lib/notify-about-routes-changes");
 
-checkOrUpdateRoutes({
+if (!process.env.GITHUB_REPOSITORY) {
+  console.log(
+    `GITHUB_REPOSITORY environment variable is missing. Must be the full name of a repository, such as "octokit/hello-world"`
+  );
+  process.exit(1);
+}
+if (!process.env.GITHUB_TOKEN) {
+  console.log(
+    `GITHUB_TOKEN environment variable is missing. Must be an installation access token with code write permission`
+  );
+  process.exit(1);
+}
+
+notifyAboutRoutesChanges({
   repoSlug: process.env.GITHUB_REPOSITORY,
   token: process.env.GITHUB_TOKEN
 });
