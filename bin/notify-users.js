74ece7685f8694a141c3e2ae20498c67b2e1d3d6
Gregor Martynus <gregor@martynus.net>
2019-10-05 21:39:49 -0700

    ci: notify users debugging

--- bin/notify-users.js
+++ bin/notify-users.js
@@ -9,61 +9,69 @@ const { Octokit } = require("@octokit/core");
 main();
 
 async function main() {
-  const auth = createAppAuth({
-    id: APP_ID,
-    privateKey: PRIVATE_KEY
-  });
-  const octokit = new Octokit({
-    auth
-  });
-
-  // TODO: paginate
-  const { data: installations } = await octokit.request(
-    "GET /app/installations",
-    { mediaType: { previews: ["machine-man"] }, per_page: 100 }
-  );
-
-  for (const {
-    id,
-    account: { login }
-  } of installations) {
-    console.log("Installation found: %s (%d)", login, id);
-
-    const { token } = await auth({
-      type: "installation",
-      installationId: id
+  try {
+    const auth = createAppAuth({
+      id: APP_ID,
+      privateKey: PRIVATE_KEY
     });
-
-    const installationOctokit = new Octokit({
-      auth: token
+    const octokit = new Octokit({
+      auth
     });
 
     // TODO: paginate
-    const {
-      data: { repositories }
-    } = await installationOctokit.request("GET /installation/repositories", {
-      mediaType: { previews: ["machine-man"] },
-      per_page: 100
-    });
-    console.log(
-      "Repositories found on %s: %d. Dispatching events",
-      login,
-      repositories.length
+    const { data: installations } = await octokit.request(
+      "GET /app/installations",
+      { mediaType: { previews: ["machine-man"] }, per_page: 100 }
     );
 
-    for (const { name, full_name } of repositories) {
-      await octokit.request("POST /repos/:owner/:repo/dispatches", {
-        mediaType: {
-          previews: ["everest"]
-        },
-        owner: login,
-        repo: name,
-        event_type: EVENT_TYPE,
-        client_payload: {
-          version: VERSION.substr(1) // v24.1.2 -> 24.1.2
-        }
+    for (const {
+      id,
+      account: { login }
+    } of installations) {
+      console.log("Installation found: %s (%d)", login, id);
+
+      const { token } = await auth({
+        type: "installation",
+        installationId: id
       });
-      console.log("Event distpatched for %s", full_name);
+
+      const installationOctokit = new Octokit({
+        auth: token
+      });
+
+      // TODO: paginate
+      const {
+        data: { repositories }
+      } = await installationOctokit.request("GET /installation/repositories", {
+        mediaType: { previews: ["machine-man"] },
+        per_page: 100
+      });
+      console.log(
+        "Repositories found on %s: %d. Dispatching events",
+        login,
+        repositories.length
+      );
+
+      for (const { name, full_name } of repositories) {
+        await installationOctokit.request(
+          "POST /repos/:owner/:repo/dispatches",
+          {
+            mediaType: {
+              previews: ["everest"]
+            },
+            owner: login,
+            repo: name,
+            event_type: EVENT_TYPE,
+            client_payload: {
+              version: VERSION.substr(1) // v24.1.2 -> 24.1.2
+            }
+          }
+        );
+        console.log("Event distpatched for %s", full_name);
+      }
     }
+  } catch (error) {
+    console.log(error);
+    throw error;
   }
 }
