b46deb0b6986d4e489f8d44848dae853b806aad7
Gregor Martynus <gregor@martynus.net>
2020-01-29 13:36:46 -0800

    test: add more logs to test:ci script

--- bin/run-tests.js
+++ bin/run-tests.js
@@ -4,6 +4,7 @@ const { resolve: pathResolve } = require("path");
 const { readdirSync } = require("fs");
 const { spawn } = require("tap");
 
+console.log(`tap --no-coverage --timeout=60 test/unit/*-test.js`);
 spawn("tap", ["--no-coverage", "--timeout=60", "test/unit/*-test.js"], {
   name: "unit tests",
   env: { PATH: process.env.PATH }
@@ -13,6 +14,11 @@ spawn("tap", ["--no-coverage", "--timeout=60", "test/unit/*-test.js"], {
 const apis = readdirSync(pathResolve(__dirname, "..", "openapi"));
 for (const api of apis) {
   const GHE_VERSION = parseFloat(api.replace(/^ghe-(\d+\.\d+)$/, "$1")) || null;
+  console.log(
+    `${
+      GHE_VERSION ? `GHE_VERSION=${GHE_VERSION} ` : ""
+    }tap --no-coverage --timeout=60 test/unit/*-test.js`
+  );
   spawn(
     "tap",
     ["--no-coverage", "--timeout=60", "test/integration/*-test.js"],
